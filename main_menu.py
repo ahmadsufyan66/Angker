4281ce23c593058ad885c014f3691756265ae564
Muhammad Aqil <aqilroblox@gmail.com>
2024-05-02 15:07:35 +0800

    completing-main_menu

--- main_menu.py
+++ main_menu.py
@@ -1,42 +1,70 @@
 import pygame
+import button
 
 #create main menu window
-SCREEN_HEIGHT , SCREEN_WIDTH = 1050, 1680
+SCREEN_WIDTH , SCREEN_HEIGHT = 1680, 1050
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Button Demo')
+pygame.init()
 
 #load button images
-start_img = pygame.image.load('start_btn.jpeg').convert_alpha()
-exit_img = pygame.image.load('exit_btn.jpeg').convert_alpha()
+start_img = pygame.image.load('start_btn-removebg-preview.png').convert_alpha()
+exit_img = pygame.image.load('exit_btn-removebg-preview.png').convert_alpha()
 
 #button class
 class Button():
     def __init__(self, x, y, image, scale):
         width = image.get_width()
         height = image.get_height()
-        self.image = pygame.transform.scale(image, (500, 800))
-        self.image = image
+        self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
+        self.clicked = False
 
-    def draw(self):
+    def draw(self, surface):
+        action = False
+        #get mouse position
+        pos = pygame.mouse.get_pos()
+        
+        #check mouseover and clicked conditions
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                action = True
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
         #draw button on screen
-        screen.blit(self.image, (self.rect.x, self.rect.y))
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+
+        return action 
 
 
 #create button instances
-start_button =Button(100, 200, start_img, 0.5)
-exit_button = Button(650, 200, exit_img, 0.5)
+start_button = button.Button(300, 400, start_img, 1.30)
+exit_button = button.Button(950, 400, exit_img, 0.5)
+
+text_font = pygame.font.Font("arial.ttf", 100)
+
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    text_rect = img.get_rect(center=(840, 250))
+    screen.blit(img, text_rect)
 
 #game loop
 run = True
 while run:
 
-    screen.fill((202, 228, 241))
+    screen.fill((255, 20, 255))
 
-    start_button.draw()
-    exit_button.draw()
+    draw_text("MAIN MENU", text_font, (0, 0, 0), 400, 600)
+
+    if start_button.draw(screen):
+        print('START')
+    if exit_button.draw(screen):
+        print('EXIT')
 
     #event handler
     for event in pygame.event.get():
